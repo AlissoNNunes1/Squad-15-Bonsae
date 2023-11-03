9a51cc46185b3f55c53b43a8dd2e5637ae530f15
marcosnv12 <127902898+marcosnv12@users.noreply.github.com>
2023-11-02 22:02:03 -0300

    .

--- bonsae/src/components/dropdown/Dropdown.js
+++ bonsae/src/components/dropdown/Dropdown.js
@@ -1,23 +1,39 @@
 import styles from "./Dropdown.module.css";
-import {useState} from 'react'
-
+import { useState, useEffect, useRef } from 'react';
 
 function Dropdown(props) {
-    const [menuIsOpen, setMenuIsOpen] = useState(false);
-    function exibirOpcoes() {
-        setMenuIsOpen(true);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setMenuIsOpen(false);
       }
-    
-    
-  
+    }
+
+    if (menuIsOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    } else {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [menuIsOpen]);
+
+  function exibirOpcoes() {
+    setMenuIsOpen(!menuIsOpen);
+  }
 
   return (
-    <div className={styles.dropdownContainer}>
+    <div className={styles.dropdownContainer} ref={dropdownRef}>
       <button onClick={exibirOpcoes} className={styles.dropdownButton}>
         Filtrar por
       </button>
       <div className={styles.dropdownImage}>
-        <svg
+        { <svg
           xmlns="http://www.w3.org/2000/svg"
           width="23"
           height="23"
@@ -28,30 +44,35 @@ function Dropdown(props) {
             d="M13.4435 16.5826H9.80041L9.80041 14.7611H13.4435V16.5826ZM19.819 5.65327V7.47483L3.42496 7.47483V5.65327L19.819 5.65327ZM17.0866 12.0287L6.15729 12.0287V10.2072L17.0866 10.2072V12.0287Z"
             fill="#1161D8"
           />
-        </svg>
-        </div>
-        {menuIsOpen && (
+        </svg>}
+      </div>
+      {menuIsOpen && (
         <div className={styles.dropdownMenuContainer}>
-        <ul className={styles.dropdownMenu}>
-                <li>
-            <a>Opção 1</a>
-          </li>
-          <div className={styles.dropdownSeparator} />
-          <li>
-            <a>Opção 2</a>
-          </li>
-          <div className={styles.dropdownSeparator} />
-          <li>
-            <a>Opção 3</a>
-          </li>
-        </ul>
+          <ul className={styles.dropdownMenu}>
+            <li>
+              <a>Opção 1</a>
+            </li>
+            <div className={styles.dropdownSeparator} />
+            <li>
+              <a>Opção 2</a>
+            </li>
+            <div className={styles.dropdownSeparator} />
+            <li>
+              <a>Opção 3</a>
+            </li>
+            <div className={styles.dropdownSeparator} />
+            <li>
+              <a>Opção 4</a>
+            </li>
+            <div className={styles.dropdownSeparator} />
+            <li>
+              <a>Opção 5</a>
+            </li>
+          </ul>
         </div>
       )}
-        
-      </div>
-      
-    
-  )
+    </div>
+  );
 }
 
 export default Dropdown;
