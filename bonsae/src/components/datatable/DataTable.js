0756982a2e3b8868d288da8f52867f894d662e82
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2023-11-13 20:12:24 -0300

    .

--- bonsae/src/components/datatable/DataTable.js
+++ bonsae/src/components/datatable/DataTable.js
@@ -3,6 +3,10 @@ import styles from "./DataTable.module.css"
 function DataTable() {
     return (
         <div className={styles.background}></div>
+
+        // props :quantas colunas,cabecalhos
+        //
+
     )
 }
 
