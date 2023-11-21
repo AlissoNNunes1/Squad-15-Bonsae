3f759ee941f52ba2010ba7b5912535676e74e500
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2023-11-21 01:01:04 -0300

    titulo add

--- bonsae/src/App.js
+++ bonsae/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import DataTable from './components/datatable/DataTable';
 import Dropdown from './components/dropdown/Dropdown'
+import Titulo from './components/titulo/Titulo';
 
 function App() {
   return (
     <div className="App">
-      <Dropdown  />
+      <Dropdown   />
+      <Titulo></Titulo>
 
 
     </div>
--- bonsae/src/components/titulo/Titulo.js
+++ bonsae/src/components/titulo/Titulo.js
@@ -1,7 +1,27 @@
-function Titulo() {
-    return (
-        <div></div>
-    )
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from "./Titulo.module.css";
+
+function Titulo({ backgroundColor, textColor, fontSize }) {
+  const tituloStyles = {
+    backgroundColor: backgroundColor || 'transparent',
+    color: textColor || '#000',
+    fontSize: fontSize || '24px',
+    // Adicione outras propriedades de estilo conforme necessário
+  };
+
+  return (
+    <div className={styles.titulo} style={tituloStyles}>
+      <h2>Título da página</h2>
+    </div>
+  );
 }
 
-export default Titulo
+Titulo.propTypes = {
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  fontSize: PropTypes.string,
+  // Adicione outras propTypes para outras opções de personalização
+};
+
+export default Titulo;
